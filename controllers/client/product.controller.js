7bc4f2417f771017c39030ea94288b567336b3de
pvduy23052005 <162944866+pvduy23052005@users.noreply.github.com>
2024-08-20 09:57:47 +0700

    view-products-for-client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,8 +1,21 @@
 // lay ve data
 const product = require("../../model/product.model.js"); 
-module.exports.index = ( req, res) => { 
+
+module.exports.index = async ( req, res) => { 
    
+   const find = {
+      hienThi : false ,
+   }
+   const data = await product.find(find).sort({position : "desc"}); 
+
+      // tinh lai gia moi . 
+      const newData = data.map((item) => { 
+         item.giaMoi = (item.gia*(100 - item.giam)/100).toFixed(0); 
+         return item ; 
+      });
+
    res.render("client/pages/products/index.pug" , {
-      
+      pageTitle : "Trang san pham",
+      product : newData
    })
 }
